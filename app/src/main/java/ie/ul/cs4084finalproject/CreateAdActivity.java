9e0999e83b68b092dc0a19b4f55f220faa9529af
SkepticQuark <roddy15.lego@gmail.com>
2020-03-15 19:27:48 +0000

    Added create ad activity.

---
+++ app/src/main/java/ie/ul/cs4084finalproject/CreateAdActivity.java
@@ -0,0 +1,14 @@
+package ie.ul.cs4084finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_ad);
+    }
+}
