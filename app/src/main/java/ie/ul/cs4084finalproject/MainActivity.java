f313b3f601adfd7143a86509dc1d1553f2522856
Adam O'Brien <17226864@studentmail.ul.ie>
2020-03-17 11:15:45 +0000

    Set up recyclerview for home page Added links buttons on home page Added slight layout changes to CreateAdActivity

--- app/src/main/java/ie/ul/cs4084finalproject/MainActivity.java
+++ app/src/main/java/ie/ul/cs4084finalproject/MainActivity.java
@@ -1,17 +1,64 @@
 package ie.ul.cs4084finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private static final String TAG = "MainActivity";
+
+    private ArrayList<Advertisement> ads = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, SearchActivity.class);
-        startActivity(intent);
+        Button createAd = findViewById(R.id.createAdButton);
+        Button search = findViewById(R.id.searchButton);
+
+        createAd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CreateAdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(i);
+            }
+        });
+
+        initAdvertisements();
+    }
+
+    private void initAdvertisements() {
+        // TODO : Load advertisements from database
+        ads.add(new Advertisement("LawnMover", "image src", 349.99, "Brand New", 12, "Adam"));
+        ads.add(new Advertisement("LawnMover 2", "image src", 389.99, "Used", 54, "Ricky"));
+        ads.add(new Advertisement("LawnMover 3", "image src", 49.99, "Used", 623, "Morty"));
+    
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        Log.d(TAG, "initRecyclerView: init Home Screen RecyclerView");
+        RecyclerView recyclerView = findViewById(R.id.homeRecyclerView);
+        SearchRecyclerViewAdapter adapter = new SearchRecyclerViewAdapter(ads);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
