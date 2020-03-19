9e48f17a04a8337a6068045b579839bd2d679fdb
Adam O'Brien <17226864@studentmail.ul.ie>
2020-03-19 13:33:17 +0000

    Home page now displays advertisements from firebase database

--- app/src/main/java/ie/ul/cs4084finalproject/MainActivity.java
+++ app/src/main/java/ie/ul/cs4084finalproject/MainActivity.java
@@ -1,5 +1,6 @@
 package ie.ul.cs4084finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,11 +11,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     
     private static final String TAG = "MainActivity";
+
+    FirebaseFirestore db;
 
     private ArrayList<Advertisement> ads = new ArrayList<>();
 
@@ -23,8 +37,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = FirebaseFirestore.getInstance();
+
         Button createAd = findViewById(R.id.createAdButton);
         Button search = findViewById(R.id.searchButton);
+
+        Button addDoc = findViewById(R.id.addDoc);
 
         createAd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,16 +60,63 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        addDoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> advertisement = new HashMap<>();
+                advertisement.put("title", "Lawnmower");
+                advertisement.put("image_src", "link-to-img");
+                advertisement.put("price", 359.99);
+                advertisement.put("quality", "Brand New");
+                advertisement.put("distance", 55);
+                advertisement.put("seller", "Adam");
+
+                db.collection("advertisements")
+                        .add(advertisement)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "Document added with ID : " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+            }
+        });
+
         initAdvertisements();
     }
 
     private void initAdvertisements() {
         // TODO : Load advertisements from database
-        ads.add(new Advertisement("LawnMover", "image src", 349.99, "Brand New", 12, "Adam"));
-        ads.add(new Advertisement("LawnMover 2", "image src", 389.99, "Used", 54, "Ricky"));
-        ads.add(new Advertisement("LawnMover 3", "image src", 49.99, "Used", 623, "Morty"));
-    
-        initRecyclerView();
+        db.collection("advertisements")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.get("seller"));
+                                ads.add(new Advertisement(
+                                        document.get("title").toString(),
+                                        document.get("image_src").toString(),
+                                        (Double)document.get("price"),
+                                        document.get("quality").toString(),
+                                        ((Long)document.get("distance")).intValue(),
+                                        document.get("seller").toString()
+                                ));
+                            }
+
+                            initRecyclerView();
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 
     private void initRecyclerView() {
